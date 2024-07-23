a6831cb3496ecb6df962037548e70ad8ce1418fa
leoseis <eboscrib2@yahoo.com>
2024-07-23 14:08:48 +0100

    added a an admin display

--- noteapp/admin.py
+++ noteapp/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Note
 # Register your models here.
 
-admin.site.register(Note)   
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ["title", "category", "created", "updated"]
+
+admin.site.register(Note, NoteAdmin)
