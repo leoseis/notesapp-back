7aa68c844fcffd92ccf68b2ad960c3d2f9c150e0
leoseis <eboscrib2@yahoo.com>
2024-08-24 16:33:40 +0100

    added cors so domians can interact configued at the settings.py

--- notes/settings.py
+++ notes/settings.py
@@ -38,12 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'noteapp',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders',
+
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -70,6 +73,12 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'notes.wsgi.application'
+
+
+CORS_ALLOWED_ORIGINS = [
+ 'http://localhost:3000' ,
+ 'http://localhost:3000 ',
+]
 
 
 # Database
