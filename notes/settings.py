adb68e7c0b51ec41f1b7775f37ab9d48ebb85491
leoseis <eboscrib2@yahoo.com>
2024-11-16 21:22:57 +0100

    final code and postgresql connected

--- notes/settings.py
+++ notes/settings.py
@@ -86,11 +86,14 @@ CORS_ALLOWED_ORIGINS = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':  'Notes',
+        'USER': 'postgres',
+        'PASSWORD': 'sparkle1',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
